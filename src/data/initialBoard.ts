f7b25af9dba28de311602b0aa81965a286f9447e
ghazale <ghazal7988@gmail.com>
2026-02-14 00:27:26 +0330

    refactor: extract initial board data from Board component

---
+++ src/data/initialBoard.ts
@@ -0,0 +1,22 @@
+export const initialBoardData = [
+    {
+        id: '1',
+        title: 'Todo',
+        cards: [
+            { id: '1', title: 'Create interview Kanban' },
+            { id: '2', title: 'Review Drag & Drop' }
+        ]
+    },
+    {
+        id: '2',
+        title: 'In Progress',
+        cards: [
+            { id: '3', title: 'Set up Next.js project' }
+        ]
+    },
+    {
+        id: '3',
+        title: 'Done',
+        cards: []
+    }
+];
